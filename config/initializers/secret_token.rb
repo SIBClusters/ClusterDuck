2eb2d19078cafcb1aaef2ea91d9d7d6efe333885
Matt Williams <technovangelist@gmail.com>
2013-03-20 14:42:32 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClusterDuck::Application.config.secret_token = 'cba7728894869223eb2e530493bd20fa41672c4a81c88b0fe51b231042330232b360364dd3edfe1d08527c3a4482806bd4a85f2434619cf15026a8b9ad026af1'
