82cf7df17b7d7e7cec109e1b6fe51def358e1359
German Velasco <germsvel@gmail.com>
2013-03-25 16:10:28 -0400

    removing feed model and controller

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,2 +1,13 @@
 class StatusesController < ApplicationController
+
+  def index
+    users = ['technovangelist','germsvel','ccoughlin_']
+    @group_feed = []
+
+    users.each do |user|
+      timeline = Feed.get_twitter_timeline(user)
+      @group_feed += timeline     
+    end
+  end
+
 end
