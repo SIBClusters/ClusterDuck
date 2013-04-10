06a42fde6c324881034824c6e37159143cb90b6d
German Velasco <germsvel@gmail.com>
2013-04-10 16:59:10 -0400

    add ability to become member of a group

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,25 +1,35 @@
 class MembersController < ApplicationController
 
+
   layout 'main'
 
   def index
     @group = Group.find(params[:group_id])
     @members = @group.members.all
+    @users = User.all
   end
 
   def new
     @group = Group.find(params[:group_id])
+    @users = User.all
     @member = Member.new
   end
 
   def create
-    @member = Member.new(params[:member])
+    group_id = params[:group_id]
+    user_id = current_user.id
+    @member = Member.new({:user_id => user_id, :group_id => group_id})
 
-    if @member.save 
-      flash[:notice] = "Thank you for adding a user"
+    if @member.already_member?(group_id, current_user.id)
+      flash[:notice] = "You are already a member of this group"
       redirect_to group_members_path(params[:group_id])
     else
-      render 'new'
+      if @member.save 
+        flash[:notice] = "Thank you for joining"
+        redirect_to group_members_path(params[:group_id])
+      else
+        render 'new'
+      end
     end
   end
 
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,15 @@
 class Member < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :user_id, :group_id
 
   belongs_to :users  
   belongs_to :groups
+
+
+  #function to check if already a member
+  def already_member?(group_id, user_id)
+    !Member.where("group_id = ? AND user_id = ?", group_id, user_id).empty?
+  end
+
+
+  #function to get member names instead of ids
 end
